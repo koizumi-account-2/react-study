24112a2badf8fcb56b5ae913ed5950422d2d65bc
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-01-24 18:40:37 +0900

    Revert "ミスの修正"
    This reverts commit 58420dde5805443cb3bec5952d3cbe6366bf5d77.

--- react-test/src/components/Counter.tsx
+++ react-test/src/components/Counter.tsx
@@ -8,8 +8,7 @@ export const Counter = () => {
         console.log(`clickEvent start count:${count}`);
         // const newCounnt = count + 1;
         // setCount(newCounnt)
-        setCount(prev => prev + 1);
-        // setCount(prev => prev++);
+        setCount(prev => prev++);
         console.log(`clickEvent end count:${count}`);
     }
     console.log("B")
