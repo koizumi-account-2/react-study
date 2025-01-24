42f7b4a0c6a25a0953111c2ed2585d72c8ec4099
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-01-24 18:32:33 +0900

    useState

---
+++ react-test/src/components/Counter.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export const Counter = () => {
+    console.log("A")
+    const [count,setCount] = useState<number>(0);
+
+    const countUp = ()=>{
+        console.log(`clickEvent start count:${count}`);
+        // const newCounnt = count + 1;
+        // setCount(newCounnt)
+        setCount(prev => prev++);
+        console.log(`clickEvent end count:${count}`);
+    }
+    console.log("B")
+    return (
+        <>
+            <div>Counter:{count}</div>
+            <button onClick={countUp}>+</button>
+        </>
+    )
+}
