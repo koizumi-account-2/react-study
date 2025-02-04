bdfdaa112dc22808dceb69e8ea879515d2749e37
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-02-04 22:14:13 +0900

    FoodList-update

--- react-test/src/foods/Food.tsx
+++ react-test/src/foods/Food.tsx
@@ -1,19 +1,43 @@
+import { useEffect, useState } from "react";
 import { useFoodListCotext, useFoodListDispatchContext, useSelectedIdContext } from "./FoodListProvider"
-
+import { FoodType } from "./type";
+const defaultFood:FoodType = {
+    id:"",
+    name:"",
+    price:0
+}
 export const Food = () => {
     const selectedFoodId = useSelectedIdContext();
     const foodList = useFoodListCotext();
     const foodListDispatch = useFoodListDispatchContext();
     const food = foodList?.find(food => food.id === selectedFoodId);
 
+
+    const [editFood , setEditFood] = useState<FoodType>(food || defaultFood);
+    useEffect(()=>{
+        setEditFood(food || defaultFood)
+    },[food])
+
+
+    const changeHandler = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        setEditFood(prev =>({...prev,[e.target.name]:e.target.value}));
+    }
+
+    const clickHandler = ()=>{
+        console.log(editFood)
+        if(foodListDispatch)foodListDispatch(prev => prev.map(item => item.id === editFood.id?editFood:item));
+    }
+
     return (
         <>
             <h2>選択したFood</h2>
             {food ? 
                 <div>
-                    <p>ID : {food.id}</p>
-                    <p>名前 : {food.name}</p>
-                    <p>値段 : {food.price}</p> 
+                    <p>ID : {editFood.id}</p>
+
+                    <p>名前</p><input type="text" name="name" value={editFood.name} onChange={changeHandler}/>
+                    <p>値段</p><input type="text" name="price" value={editFood.price} onChange={changeHandler}/>
+                    <p><button onClick={clickHandler}>保存</button></p>
                 </div>
             :
             <>なし</>}
