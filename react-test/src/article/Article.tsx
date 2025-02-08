ca9848ec039c607be6e7a4348cdc1eaffbc4163f
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-02-09 08:25:39 +0900

    イベント発生時の非同期処理

--- react-test/src/article/Article.tsx
+++ react-test/src/article/Article.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 type TArticle =  {
     userId:number,
@@ -9,30 +9,64 @@ type TArticle =  {
 
 
 export const Article = () => {
-    const [articles,setArticles] = useState<TArticle[]>([]);
-    useEffect(()=>{
-        const getArticles = async()=>{
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const articles:TArticle[] = await response.json();
-            setArticles(articles);
-        }
-        getArticles();
-    },[])
+    const [articleState,setArticle] = useState<TArticle | null>(null);
+    const articleIdList:number[] = [0,1,2,3,4,5]
+    
+    const getArticle = async(postId:string):Promise<TArticle|null>=>{
+        if(postId === '0') return null
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+        const article:TArticle = await response.json();
+        return article;
+    }
+    const changeHandler = async (e:React.ChangeEvent<HTMLSelectElement>)=>{
+        const article = await getArticle(e.target.value);
+        console.log(article);
+        setArticle(article)
+    }   
     return (
-        <>
+        <div>
+            <select onChange={changeHandler}>
+                {articleIdList.map(id => <option key={id} value={id}>{id>0?id:"未選択"}</option>)}
+            </select>
+
             {
-                articles.map(article =>             
-                    <div key={article.id}>
-                        <h3>{article.title}</h3>
-                        <div>{article.body}</div>
-                        <hr/>
-                    </div>
-                )
-
+                articleState && 
+                <div>
+                    <span>{articleState.title}</span>
+                    <div>{articleState.body}</div>
+                </div>
             }
-
-        </>
+        </div>
     )
 }
 
 
+    // const [articleState,setArticle] = useState<TArticle | null>(null);
+    // const articleIdList:number[] = [0,1,2,3,4,5]
+    
+    // const getArticle = async(postId:string):Promise<TArticle|null>=>{
+    //     if(postId === '0') return null
+    //     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    //     const article:TArticle = await response.json();
+    //     return article;
+    // }
+    // const changeHandler = async (e:React.ChangeEvent<HTMLSelectElement>)=>{
+    //     const article = await getArticle(e.target.value);
+    //     console.log(article);
+    //     setArticle(article)
+    // }   
+    // return (
+    //     <div>
+    //         <select onChange={changeHandler}>
+    //             {articleIdList.map(id => <option key={id} value={id}>{id>0?id:"未選択"}</option>)}
+    //         </select>
+
+    //         {
+    //             articleState && 
+    //             <div>
+    //                 <span>{articleState.title}</span>
+    //                 <div>{articleState.body}</div>
+    //             </div>
+    //         }
+    //     </div>
+    // )
